7526e331c8e13210845b35163c90d7310ac3331f
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-07-18 16:06:17 -0500

    saving work

--- src/main/java/beans/BeanTest.java
+++ src/main/java/beans/BeanTest.java
@@ -7,11 +7,14 @@ import java.util.Date;
 public class BeanTest{
 
     public static void main(String[] args) {
+//        Artist + Album
     Artist JCole = new Artist(17,false, "JCole", "English");
     Album TheOffSeason = new Album(16, JCole, "The Off-Season", new Date("05/14/2021"), 1.0, "Rap");
 
         System.out.println(TheOffSeason.getName() + " by " + TheOffSeason.getArtist().getName() + "\n" + "Release Date: " + TheOffSeason.getReleaseDate() + "\n" + "Genre: " + TheOffSeason.getGenre() + "\n" + "Album Sales: " + TheOffSeason.getSales() + "\n");
 
+
+//        Author + Quote
         Author Romeo = new Author(1, "Romeo", "Cohens");
         Author Jack = new Author(2, "Jacqueline", "Camacho");
         Quote quote1 = new Quote(1, "Failure is a part of Success!", Romeo);
@@ -26,9 +29,5 @@ public class BeanTest{
         for (Quote quote : quotes) {
             System.out.println(quote.getAuthor().getFirstName() + quote.getAuthor().getLastName() + " ~ " + quote.getContent());
         }
-
-
-
-
     }
 }
