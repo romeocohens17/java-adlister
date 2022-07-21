9558778ba93c842278d957fcbb22861380d22732
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-07-21 12:54:44 -0500

    saving jdbc-exercise

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+public class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "root";
+    }
+    public String getPassword() {
+        return "codeup";
+    }
+}
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+
+    @Override
+    public List<Ad> all() {
+       List<Ad> newAdsList = new ArrayList<>();
+       try {
+           Statement smt = connection.createStatement();
+           ResultSet rs = smt.executeQuery("SELECT * FROM ads");
+           while(rs.next()) {
+               newAdsList.add(new Ad(
+                       rs.getLong("id"),
+                       rs.getLong("user_id"),
+                       rs.getString("title"),
+                       rs.getString("description")
+               ));
+           }
+           return newAdsList;
+       } catch (SQLException e) {
+           throw new RuntimeException("Error Connecting to Database", e);
+       }
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        String title = ad.getTitle();
+        String desc = ad.getDescription();
+        String query = "INSERT INTO ads(user_id, title, description) VALUES(1, '"+title+"', '"+desc+"')";
+        try {
+            Statement smt = connection.createStatement();
+            smt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = smt.getGeneratedKeys();
+            rs.next();
+            System.out.println("Created a new ad, id of the ad: " + rs.getLong(1));
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error Connecting to Database", e);
+        }
+    }
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword());
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new RuntimeException("Error Connecting to Database", e);
+        }
+    }
+}
