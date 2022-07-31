b8386c4842a63cd24392696e9fc23e26ee5496ab
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-07-31 01:07:36 -0500

    saving servlets exercise

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,13 +1,15 @@
-package com.codeup.adlister.dao;
-
-public class DaoFactory {
-    private static Ads adsDao;
-    private static Config config = new Config();
-
-    public static Ads getAdsDao() {
-        if (adsDao == null) {
-            adsDao = new MySQLAdsDao(config);
-        }
-        return adsDao;
-    }
-}
+//package com.codeup.adlister.dao;
+//
+//import javax.servlet.jsp.jstl.core.Config;
+//
+//public class DaoFactory {
+//    private static Ads adsDao;
+//    private static Config config = new Config();
+//
+//    public static Ads getAdsDao() {
+//        if (adsDao == null) {
+//            adsDao = new MySQLAdsDao(config);
+//        }
+//        return adsDao;
+//    }
+//}
